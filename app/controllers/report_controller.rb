eb715ac127032b30ef75497a3af3dc1738c0f17c
Jonathan Rochkind <jonathan@dnil.net>
2012-08-21 12:17:32 -0400

    more hacky things in reports

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,6 +2,8 @@ class ReportController < ApplicationController
   
   def index
     @results = fetch_stats
+    @match_up_keys, @match_up_count = fetch_sanity_check_grid 
+    @demographic_overview = fetch_demographic_overview    
   end
   
   protected
@@ -43,6 +45,55 @@ class ReportController < ApplicationController
    EOS
    
    return Selection.connection.select_all( sql )      
+  end
+  
+  # again some raw sql, a grid of how often each engine was faced
+  # off against each other engine in a match that resulted in
+  # a a/b/tie selection. 
+  def fetch_sanity_check_grid
+    results = Selection.connection.select_all("select option_a, option_b, count(*) as count from selections group by option_a, option_b")
+    # we get back pairings that are A vs B in one row, and B vs A in a different one. Combine em all. 
+    
+    match_ups = Hash.new do |h, k|
+      h[k] = Hash.new(0)
+    end
+
+    results.each do |row|
+      normal = [row["option_a"], row["option_b"]].sort
+      match_ups[normal.first][normal.last] += row["count"]
+      match_ups[normal.last][normal.first] += row["count"]
+    end
+    
+    engines = Selection.connection.
+      select_all("select option_a as engine from selections union select option_b as engine from selections").
+      collect {|row| row["engine"]}
+
+    return engines, match_ups
+    
+  end
+  
+  def fetch_demographic_overview
+    sql = <<-EOS
+      select demographic_school, demographic_status, count(*) as count
+      from selections 
+      group by demographic_school, demographic_status
+      
+      union all
+      
+      select demographic_school, "TOTAL" as demographic_status, count(*) as count
+      from selections
+      group by demographic_school
+      
+      union all
+      
+      select "TOTAL" as demographic_school, demographic_status, count(*) as count
+      from selections
+      group by demographic_status
+      EOS
+      
+      results = Selection.connection.select_all(sql)
+
+      return results
   end
   
 end
